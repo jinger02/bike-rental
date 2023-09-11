a44db446436975e8f51ddee5702cd9ea49b3a95b
jinger02 <maryjane.galindo@gmail.com>
2023-09-11 02:01:57 -0400

    Save progress: 1280.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -66,6 +66,7 @@ RENT_MENU(){
                 then
                     # get new customer name
                     echo -e "\nWhat's your name?"
+                    read CUSTOMER_NAME
                     # insert new customer
                 fi
             fi
