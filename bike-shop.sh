fd3d508b980f7539d626cb6fd926a1a833f92b79
jinger02 <maryjane.galindo@gmail.com>
2023-09-11 21:31:12 -0400

    Save progress: 1474.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -94,8 +94,10 @@ RETURN_MENU(){
     read PHONE_NUMBER
     CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$PHONE_NUMBER'")
     # if not found
-    # send to main menu
-    
+    if [[ -z $CUSTOMER_ID ]]
+    then
+        # send to main menu
+    fi
 }
 EXIT(){
     echo -e "\nThank you for stopping in.\n"
